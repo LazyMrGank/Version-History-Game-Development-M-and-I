9ad0f18699d2229706c023de964a062ac55811da
LazyMrGank <100172063+LazyMrGank@users.noreply.github.com>
2025-06-27 09:46:51 +1200

    Settings done

--- godot-game-world/pause_menu.gd
+++ godot-game-world/pause_menu.gd
@@ -23,7 +23,7 @@ func _on_button_pressed() -> void:
 
 
 func _on_button_2_pressed() -> void:
-	get_tree().reload_current_scene()
+	get_tree().change_scene_to_file("res://settings.tscn")
 
 
 func _on_button_3_pressed() -> void:
