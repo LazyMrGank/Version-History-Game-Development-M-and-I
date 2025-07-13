63c140b9614cc96ea254a5860ce228fbae66392f
LazyMrGank <100172063+LazyMrGank@users.noreply.github.com>
2025-07-14 11:00:55 +1200

    Health bar connected to HUD

--- playermovement-test/healthbar.gd
+++ playermovement-test/healthbar.gd
@@ -1,9 +1,27 @@
 extends Control
 
+@onready var health = $"../Health"
+@onready var health_bar = $TextureProgressBar
 
 
-func _on_h_slider_value_changed(value: float) -> void:
-	$TextureProgressBar.value = value
-	$TextureProgressBar2.value = value
-	$TextureProgressBar3.value = value
-	$TextureProgressBar4.value = value
+func _on_health_depleted():
+	health_bar.value = 0
+	
+func _on_health_health_changed(diff: int) -> void:
+	health_bar.value = health.get_health()
+	pass # Replace with function body.
+
+
+func _on_health_max_health_changed(diff: int) -> void:
+	health_bar.max_value = health.get_max_health()
+	pass # Replace with function body.
+	
+func _ready():
+	# Connect signals
+	health.health_changed.connect(_on_health_health_changed)
+	health.max_health_changed.connect(_on_health_max_health_changed)
+	health.health_depleted.connect(_on_health_depleted)
+	
+	# Initialize health bar
+	health_bar.max_value = health.get_max_health()
+	health_bar.value = health.get_health()
