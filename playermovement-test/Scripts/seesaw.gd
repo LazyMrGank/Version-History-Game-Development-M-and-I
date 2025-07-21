8fbeec5ac0d420f3190a0b2283299e8b84ab0b8c
LazyMrGank <100172063+LazyMrGank@users.noreply.github.com>
2025-07-22 10:29:15 +1200

    ACTUALLY FIXED

--- playermovement-test/Scripts/seesaw.gd
+++ playermovement-test/Scripts/seesaw.gd
@@ -6,8 +6,8 @@ var bodies_on_seesaw = 0  # Track how many bodies are on the seesaw
 
 func _ready():
 	# Connect the body_entered and body_exited signals for collision detection
-	$CollisionShape2D.connect("body_entered", _on_body_entered)
-	$CollisionShape2D.connect("body_exited", _on_body_exited)
+	body_entered.connect(_on_body_entered)
+	body_exited.connect(_on_body_exited)
 
 func _physics_process(delta):
 	# Only recenter if no bodies are on the seesaw
