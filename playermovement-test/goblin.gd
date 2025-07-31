13f07a2d5a963aabc0ab114cd6d40a7969528f50
LazyMrGank <100172063+LazyMrGank@users.noreply.github.com>
2025-07-31 12:23:05 +1200

    Attempted to fix enemy

--- playermovement-test/goblin.gd
+++ playermovement-test/goblin.gd
@@ -4,30 +4,37 @@ var gravity = 10
 var speed = 32
 
 func _ready():
-	$AnimationPlayer.play("Walk")
+	$Visuals/AnimationPlayer.play("Walk")
 	
 func _physics_process(_delta):
+	if $Visuals/AnimationPlayer.current_animation == "Attack":
+		return
 	move_character()
 	detect_turn_around()
-	hit()
-	end_of_hit()
-	start_walk()
+	
 func move_character():
-	velocity.x = -speed if is_moving_left else speed
+	velocity.x = speed if is_moving_left else -speed
 	velocity.y += gravity
 	move_and_slide()
+
 	
 func detect_turn_around():
-	if not $RayCast2D.is_colliding() and is_on_floor():
+	if not $Visuals/RayCast2D.is_colliding() and is_on_floor():
 		is_moving_left = !is_moving_left
 		scale.x = -scale.x
 		
+		
 func hit():
-	$AttackDetector.monitoring = true
+	$Visuals/AttackDetector.monitoring = true
 	
 func end_of_hit():
-	$AttackDetector.monitoring = false
+	$Visuals/PlayerDetector.monitoring = false
 	
 func start_walk():
-	$AnimationPlayer.play("Walk")
+	$Visuals/AnimationPlayer.play("Walk")
 	
+func _on_player_detector_body_entered(body: Node2D) -> void:
+	$Visuals/AnimationPlayer.play("Attack")
+	
+func _on_attack_detector_body_entered(body: Node2D) -> void:
+	get_tree().reload_current_scene()
