2738c1e514b2cca08cb1f3f6513f282d1f7cdd7e
Tony TOny Chopper <iantan27@kingshigh.school.nz>
2025-08-06 09:58:38 +1200

    Changed it again

--- playermovement-test/goblin.gd
+++ playermovement-test/goblin.gd
@@ -1,12 +1,15 @@
 extends CharacterBody2D
 
 @onready var animation_player = $Visuals/AnimationPlayer
+@onready var chase_detector = $Visuals/ChaseDetector  # New Area2D for chase range
 var is_moving_left: bool = true
 var gravity: float = 10.0
 var speed: float = 32.0
 var is_attacking: bool = false
 var is_hit: bool = false
-var health: int = 3  # New health variable
+var health: int = 3
+var player: Node2D = null  # Reference to the player when in chase range
+var chase_speed: float = 48.0  # Slightly faster for chasing, adjust as needed
 
 func _ready() -> void:
 	$Visuals/AnimationPlayer.play("Walk")
@@ -14,19 +17,32 @@ func _ready() -> void:
 	$Visuals/PlayerDetector.body_exited.connect(_on_player_detector_body_exited)
 	$Visuals/AttackDetector.body_entered.connect(_on_attack_detector_body_entered)
 	$Visuals/AnimationPlayer.animation_finished.connect(_on_animation_finished)
-	add_to_group("enemies")  # Ensure enemy is in "enemies" group for player detection
+	chase_detector.body_entered.connect(_on_chase_detector_body_entered)
+	chase_detector.body_exited.connect(_on_chase_detector_body_exited)
+	add_to_group("enemies")
 
 func _physics_process(_delta: float) -> void:
 	if is_hit or is_attacking or health <= 0:  # Stop movement if hit, attacking, or dead
 		velocity.x = 0
 	else:
-		move_character()
-		detect_turn_around()
+		if player:  # Chase player if detected
+			move_toward_player()
+		else:  # Patrol left and right if no player
+			move_character()
+			detect_turn_around()
 	velocity.y += gravity
 	move_and_slide()
 
 func move_character() -> void:
 	velocity.x = speed if is_moving_left else -speed
+
+func move_toward_player() -> void:
+	if player and is_on_floor():
+		var direction = sign(player.global_position.x - global_position.x)  # 1 if player is to the right, -1 if left
+		velocity.x = direction * chase_speed
+		# Flip sprite to face player
+		if direction != 0:
+			scale.x = -1 if direction < 0 else 1
 
 func detect_turn_around() -> void:
 	if not $Visuals/RayCast2D.is_colliding() and is_on_floor():
@@ -47,20 +63,27 @@ func _on_player_detector_body_exited(body: Node2D) -> void:
 
 func _on_attack_detector_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player") and is_attacking:
-		
 		print("Hit player during attack!")
+
+func _on_chase_detector_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player") and health > 0:
+		player = body  # Set player reference for chasing
+
+func _on_chase_detector_body_exited(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		player = null  # Clear player reference to resume patrolling
 
 func _on_animation_finished(anim_name: String) -> void:
 	if anim_name == "Attack" and not is_hit and health > 0:
 		is_attacking = false
 		$Visuals/AnimationPlayer.play("Walk")
 		$Visuals/AttackDetector.monitoring = false
-	elif anim_name == "hit" and health > 0:  # Resume walking if still alive
+	elif anim_name == "hit" and health > 0:
 		is_hit = false
 		is_attacking = false
 		$Visuals/AnimationPlayer.play("Walk")
 		$Visuals/AttackDetector.monitoring = false
-	elif anim_name == "Death":  # Remove enemy after death animation
+	elif anim_name == "Death":
 		queue_free()
 
 func play_hit_animation():
@@ -68,7 +91,7 @@ func play_hit_animation():
 		is_hit = true
 		is_attacking = false
 		$Visuals/AttackDetector.monitoring = false
-		health -= 1  # Reduce health by 1
+		health -= 1
 		velocity.x = 0
 		if health <= 0:
 			animation_player.play("Death")
