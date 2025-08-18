90ad2be62495132354d55cc7fc0b2c04290a7e7c
LazyMrGank <100172063+LazyMrGank@users.noreply.github.com>
2025-08-18 12:25:33 +1200

    redid the stuff that is meant to exist but decided to F off

--- playermovement-test/Scenes/Fall_Area.gd
+++ playermovement-test/Scenes/Fall_Area.gd
@@ -11,6 +11,6 @@ func _process(delta: float) -> void:
 	pass
 
 
-func _on_body_entered(body: Node2D) -> void:
+func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):#HOLY CRAP PLAYER GOT TOUCHED FOR REALIES OMG")
 		get_tree().reload_current_scene()
