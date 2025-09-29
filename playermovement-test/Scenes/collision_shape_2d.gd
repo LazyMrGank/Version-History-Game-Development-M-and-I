1eca74cc38d4df62fec72a119163d388c49eba38
LazyMrGank <100172063+LazyMrGank@users.noreply.github.com>
2025-09-29 17:35:30 +1300

    only lights left of path of pain parkour but like really?

--- playermovement-test/Scenes/collision_shape_2d.gd
+++ playermovement-test/Scenes/collision_shape_2d.gd
@@ -32,5 +32,57 @@ func _on_another_another_another_death_barrier_body_entered(body: Node2D) -> voi
 		killPlayer()
 
 
+
+
 func killPlayer():
 	player.position = checkpoint_manager.last_location
+
+
+func _on_area_2d_2_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player"):#HOLY CRAP PLAYER GOT TOUCHED FOR REALIES OMG")
+		print("no it was I")
+		killPlayer()
+
+
+func _on_area_2d_3_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player"):#HOLY CRAP PLAYER GOT TOUCHED FOR REALIES OMG")
+		print("it was I")
+		killPlayer()
+
+
+
+
+func _on_area_2d_4_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player"):#HOLY CRAP PLAYER GOT TOUCHED FOR REALIES OMG")
+		print("god it was I")
+		killPlayer()
+
+
+func _on_area_2d_5_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player"):#HOLY CRAP PLAYER GOT TOUCHED FOR REALIES OMG")
+		print("god it was I")
+		killPlayer()
+
+
+func _on_area_2d_6_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player"):#HOLY CRAP PLAYER GOT TOUCHED FOR REALIES OMG")
+		print("god it was I")
+		killPlayer()
+
+
+func _on_area_2d_7_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player"):#HOLY CRAP PLAYER GOT TOUCHED FOR REALIES OMG")
+		print("god it was I")
+		killPlayer()
+
+
+func _on_area_2d_9_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player"):#HOLY CRAP PLAYER GOT TOUCHED FOR REALIES OMG")
+		print("god it was I")
+		killPlayer()
+
+
+func _on_area_2d_15_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player"):#HOLY CRAP PLAYER GOT TOUCHED FOR REALIES OMG")
+		print("god it was I")
+		killPlayer()
